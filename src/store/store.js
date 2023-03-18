271080c3a9427930a3c8412ca2bfd0d8e42ba266
Marichka Offen <40467112+marichka-offen@users.noreply.github.com>
2023-03-18 01:27:14 -0400

    Hide logging for prod

--- src/store/store.js
+++ src/store/store.js
@@ -2,7 +2,9 @@ import { compose, createStore, applyMiddleware } from 'redux'
 import logger from 'redux-logger'
 import { rootReducer } from './rootReducer'
 
-const middleWares = [logger]
+const middleWares = [process.env.NODE_ENV === 'development' && logger].filter(
+  Boolean
+)
 const composeMiddleware = compose(applyMiddleware(...middleWares))
 
 export const store = createStore(rootReducer, undefined, composeMiddleware)
