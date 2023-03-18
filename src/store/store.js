ceb6c4fb729e8906cd8a95d7ba763ca57eaf753c
Marichka Offen <40467112+marichka-offen@users.noreply.github.com>
2023-03-18 01:18:53 -0400

    Add redux-persist to keep store state in local memory

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,20 @@
 import { compose, createStore, applyMiddleware } from 'redux'
 import logger from 'redux-logger'
 import { rootReducer } from './rootReducer'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  blacklist: ['user'],
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 const middleWares = [logger]
 const composeMiddleware = compose(applyMiddleware(...middleWares))
 
-export const store = createStore(rootReducer, undefined, composeMiddleware)
+export const store = createStore(persistedReducer, undefined, composeMiddleware)
+
+export const persistor = persistStore(store)
