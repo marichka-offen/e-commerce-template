85763b9fb16cc9d368eef18751eb49772a155509
Marichka Offen <40467112+marichka-offen@users.noreply.github.com>
2023-03-17 23:40:58 -0400

    Remove last provider...

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
-import { CartProvider } from './contexts/CartContext'
 import { Provider } from 'react-redux'
 import { store } from './store/store'
 
@@ -13,15 +12,10 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <CartProvider>
-          <App />
-        </CartProvider>
+        <App />
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
 )
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals()
