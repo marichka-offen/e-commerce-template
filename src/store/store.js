8906ff5b9c4b89c9694f0248f4dbea057d7b0bb7
Marichka Offen <40467112+marichka-offen@users.noreply.github.com>
2023-03-21 23:07:48 -0400

    Add redux-toolkit

--- src/store/store.js
+++ src/store/store.js
@@ -1,29 +1,34 @@
-import { compose, createStore, applyMiddleware } from 'redux'
+import { configureStore } from '@reduxjs/toolkit'
+// import { compose, createStore, applyMiddleware } from 'redux'
 import logger from 'redux-logger'
 import { rootReducer } from './rootReducer'
-import { persistStore, persistReducer } from 'redux-persist'
-import storage from 'redux-persist/lib/storage'
+// import { persistStore, persistReducer } from 'redux-persist'
+// import storage from 'redux-persist/lib/storage'
 
-const persistConfig = {
-  key: 'root',
-  storage,
-  blacklist: ['user'],
-}
+// const persistConfig = {
+//   key: 'root',
+//   storage,
+//   blacklist: ['user'],
+// }
 
-const persistedReducer = persistReducer(persistConfig, rootReducer)
+// const persistedReducer = persistReducer(persistConfig, rootReducer)
 
-const middleWares = [process.env.NODE_ENV !== 'production' && logger].filter(
+const middleWares = [process.env.NODE_ENV === 'development' && logger].filter(
   Boolean
 )
 
-const composeEnhancer =
-  (process.env.NODE_ENV !== 'production' &&
-    window &&
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
-  compose
+// const composeEnhancer =
+//   (process.env.NODE_ENV !== 'production' &&
+//     window &&
+//     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+//   compose
 
-const composeMiddleware = composeEnhancer(applyMiddleware(...middleWares))
+// const composeMiddleware = composeEnhancer(applyMiddleware(...middleWares))
 
-export const store = createStore(persistedReducer, undefined, composeMiddleware)
+// export const store = createStore(persistedReducer, undefined, composeMiddleware)
+export const store = configureStore({
+  reducer: rootReducer,
+  //   middleware: middleWares,
+})
 
-export const persistor = persistStore(store)
+// export const persistor = persistStore(store)
